5492199a1533a2d8dbe45599c40812706f1a3520
Harold Pimentel <haroldpimentel@gmail.com>
2016-02-18 12:40:31 -0800

    documentation for gene_common #vc

--- geuvadis/R/gene_common.R
+++ geuvadis/R/gene_common.R
@@ -1,4 +1,11 @@
-
+#' Load the differential expression truth
+#'
+#' This function loads the isoform level information and gene level information.
+#' The gene level differential expression is defined by at least 1 isoform of the gene being differentially expressed.
+#'
+#' @param sim_name the name of the simulation (with respect to the results location)
+#' @param ttg the 'target_to_gene_mapping' coming from ensembl
+#' @return a list containing the gene and isoform differential expression information
 get_de_info <- function(sim_name, ttg) {
   sims_dir <- file.path('../sims', sim_name)
   base_dir <- file.path('../results', sim_name)
@@ -22,6 +29,13 @@ get_de_info <- function(sim_name, ttg) {
   list(de_info = de_info, de_genes = de_genes)
 }
 
+#' Get gene counts from featureCounts
+#'
+#' Assumes \code{featureCounts} has been run and the results are in the appropriate place.
+#'
+#' @param sim_name the name of the simulation (with respect to the results location)
+#' @param sim_num the integer number of the simulation (with respect to \code{sim_name})
+#' @return a matrix containing gene counts
 get_gene_counts <- function(sim_name, sim_num = 1) {
   base_dir <- file.path('../results', sim_name)
 
