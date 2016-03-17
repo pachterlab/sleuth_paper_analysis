0738a4ab6c70f94d0af634f0d576bd095de1cfef
Harold Pimentel <haroldpimentel@gmail.com>
2016-03-16 19:01:22 -0700

    update the fold change distribution #vc

--- cuffdiff2_analysis/R/DESeq2.R
+++ cuffdiff2_analysis/R/DESeq2.R
@@ -1,13 +1,45 @@
 source("https://bioconductor.org/biocLite.R")
 biocLite("tximport")
 
+source('load_info.R')
+source('../../geuvadis/R/benchmark_methods.R')
+source('../../simulation_core/R/simulate_de.R')
+
 library('DESeq2')
-library('tximport')
+
+file_names <- file.path(dirname(info$path), 'featureCounts.txt')
+
+counts_list <- lapply(seq_along(file_names),
+  function(i) {
+    f <- file_names[i]
+    df <- data.table::fread(f, skip = 1, data.table = FALSE)
+    df <- dplyr::select(df, 1, 7)
+    data.table::setnames(df, c('ens_gene', info$sample[i]))
+    df
+  })
+
+counts <- Reduce(function(x, y) dplyr::left_join(x, y, by = 'ens_gene'),
+  counts_list)
+rownames(counts) <- counts$ens_gene
+counts$ens_gene <- NULL
+counts <- as.matrix(counts)
+
+pass_filter <- DESeq2_filter(counts)
+counts <- counts[pass_filter, ]
+
+rownames(info) <- info$sample
+cds <- make_count_data_set(counts, info)
+
+res <- runDESeq2(cds)
+saveRDS(res, '../results/DESeq2.rds')
+
+ds_res <- dplyr::filter(res, qval <= 0.05)
 
 
-###
-# import transcripts
-###
+sr <- dplyr::filter(sgr$sleuth.lrt, qval <= 0.05)
 
-counts <- tximport(file.path(info$path, 'abundance.tsv'),
-  type = 'kallisto', tx2gene = transcript_gene_mapping)
+s_length <- length(sr$target_id)
+d_length <- nrow(ds_res)
+
+length(intersect(ds_res$ens_gene, sr$target_id))
+length(union(ds_res$ens_gene, sr$target_id))
